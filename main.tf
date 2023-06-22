93177dd47f0fb5d714dc6469afbec6106914ff8e
brianwaithaka <brian@waithaka.me>
2023-06-21 20:40:39 -0400

    ver1 deployed storage acc and rg

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
+# Init
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "=3.6.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.6.0"
     }
   }
 }
@@ -10,5 +11,22 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+}
+
+# Resources
+resource "azurerm_resource_group" "example" {
+  name     = "tf-test-resourcegroup"
+  location = "canadacentral"
+
+}
+
+resource "azurerm_storage_account" "tf-storage-1" {
+  name                     = "brianstorageaccount98"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_kind             = "StorageV2"
+  access_tier              = "Hot"
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
 
 }
